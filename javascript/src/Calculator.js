38c4b35d7d51cc9726633b592d2aab0e14ef1ec0
Bryan Grimes <bryan@bryangrimes.com>
2012-04-19 08:25:39 -0400

    working commit

--- javascript/src/Calculator.js
+++ javascript/src/Calculator.js
@@ -1,3 +1,13 @@
 function Add(numbers) {
-	return -1;
+
+	if (numbers == "") return 0;
+
+	var nums = numbers.split(/[,|\n]/);
+	var sum = 0;
+
+	for (var i = nums.length - 1; i >= 0; i--){
+		sum += parseInt(nums[i], 10);
+	}
+	
+	return sum;
 }
