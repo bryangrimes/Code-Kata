171cb2ec45b16f5cc4c18d215fcf9d4d4f5ce740
Bryan Grimes <bryan@bryangrimes.com>
2012-04-20 09:45:17 -0400

    through step 4

--- javascript/spec/CalculatorSpec.js
+++ javascript/spec/CalculatorSpec.js
@@ -15,8 +15,14 @@ describe("Add", function() {
     expect("2,4,6,8,10").toReturnSum(30);
   });
 
-  it("should return sum with new lines and as delimeter as well", function(){
-    expect("2\n4,5").toReturnSum(11);
+  it("should return sum with commas and new lines", function(){
+    expect("1\n2,3").toReturnSum(6);
+    expect("1\n2\n3").toReturnSum(6);
+  });
+
+  it("should return sum with custom delimeter", function() {
+    expect("//@\n1@2@3").toReturnSum(6);
+    expect("//A\n1A2A3").toReturnSum(6);
   });
 
 });
--- javascript/src/Calculator.js
+++ javascript/src/Calculator.js
@@ -1,13 +1,29 @@
-function Add(numbers) {
+function parse_input(input) {
+	if (input === "") return 0;
 
-	if (numbers == "") return 0;
+	// get the delimeter out of the input
+	var delimeter = new RegExp("//(.)\n(.*)");
+	var splitter = new RegExp(/[,|\n]/);
 
-	var nums = numbers.split(/[,|\n]/);
+	// see if anything matches the delimeter created
+	var match = input.match(delimeter);
+
+	if(match){
+		// ok, so there's a custom delimeter, split on it and return the sum
+		delimeter = match[1];
+		// reset the inpur sans custom splitter
+		input = match[2];
+		// reset the splitter
+		splitter = new RegExp("[,|\n|" + delimeter + "]");
+	}
+	return input.split(splitter);
+}
+
+function Add(input) {
+	var nums = parse_input(input);
 	var sum = 0;
-
 	for (var i = nums.length - 1; i >= 0; i--){
 		sum += parseInt(nums[i], 10);
 	}
-	
 	return sum;
 }
