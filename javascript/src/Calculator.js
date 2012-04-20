f672e6efadba618a1b14375cc7bbb93b971590c9
Bryan Grimes <bryan@bryangrimes.com>
2012-04-20 09:06:44 -0400

    reset code..use branches only

--- javascript/src/Calculator.js
+++ javascript/src/Calculator.js
@@ -1,13 +1,3 @@
-function Add(numbers) {
-
-	if (numbers == "") return 0;
-
-	var nums = numbers.split(/[,|\n]/);
-	var sum = 0;
-
-	for (var i = nums.length - 1; i >= 0; i--){
-		sum += parseInt(nums[i], 10);
-	}
-	
-	return sum;
+function Add(input) {
+	return "";	
 }
