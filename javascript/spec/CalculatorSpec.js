fc10c5b37fcbf777a50bad1d0a5eb88a322a8c02
Bryan Grimes <bryan@bryangrimes.com>
2012-04-20 08:55:26 -0400

    working

--- javascript/spec/CalculatorSpec.js
+++ javascript/spec/CalculatorSpec.js
@@ -1,3 +1,14 @@
 describe("Add", function() {
+  it("should return 0 for an empty string input", function() {
+    expect("").toReturnSum(0);
+  });
+
+  it("should return a single input back", function(){
+    expect("1").toReturnSum(1);
+  });
+
+  it("",function() {
+    expect("1,2").toReturnSum(3);
+  });
 
 });
