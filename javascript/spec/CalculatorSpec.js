de6258d554a2e6b3bf4ed7505c8bd570b0596cd2
Bryan Grimes <bryan@bryangrimes.com>
2012-04-20 08:15:07 -0400

    lint clean up

--- javascript/spec/CalculatorSpec.js
+++ javascript/spec/CalculatorSpec.js
@@ -4,19 +4,19 @@ describe("Add", function() {
     });
 
   it("should return a number for an empty string", function() {
-  	expect("").toReturnSum(0);
+    expect("").toReturnSum(0);
   });
 
   it("should add two numbers together", function() {
-  	expect("2,3").toReturnSum(5);
+    expect("2,3").toReturnSum(5);
   });
 
   it("should add more than two numbers together", function() {
-  	expect("2,3,1").toReturnSum(6);
+    expect("2,3,1").toReturnSum(6);
   });
 
   it("should return sum with commas and new lines", function(){
-  	expect("1\n2,3").toReturnSum(6);
+    expect("1\n2,3").toReturnSum(6);
     expect("1\n2\n3").toReturnSum(6);
   });
 
