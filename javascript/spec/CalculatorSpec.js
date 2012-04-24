5ee0fb41ea6c0902a1afabf19928a71951bd676f
Bryan Grimes <bryan@bryangrimes.com>
2012-04-24 09:58:47 -0400

    initian branch commit

--- javascript/spec/CalculatorSpec.js
+++ javascript/spec/CalculatorSpec.js
@@ -1,3 +1,32 @@
 describe("Add", function() {
+  it("should return 0 for an empty string input", function() {
+    expect("").toReturnSum(0);
+  });
+
+  it("should return a single input back", function(){
+    expect("1").toReturnSum(1);
+  });
+
+  it("should return sum of two numbers",function() {
+    expect("1,2").toReturnSum(3);
+  });
+
+  it("should return sum of N amount of numbers", function() {
+    expect("2,4,6,8,10").toReturnSum(30);
+  });
+
+  it("should return sum with commas and new lines", function(){
+    expect("1\n2,3").toReturnSum(6);
+    expect("1\n2\n3").toReturnSum(6);
+  });
+
+  it("should return sum with custom delimeter", function() {
+    expect("//@\n1@2@3").toReturnSum(6);
+    expect("//A\n1A2A3").toReturnSum(6);
+  });
+
+  it("should throw error with single negative number", function(){
+    expect(Error);
+  });
 
 });
--- javascript/src/Calculator.js
+++ javascript/src/Calculator.js
@@ -1,3 +1,34 @@
+function parse_input(input) {
+	if (input === "") return 0;
+
+	// get the delimeter out of the input
+	var delimeter = new RegExp("//(.)\n(.*)");
+	var splitter = new RegExp(/[,|\n]/);
+
+	// see if anything matches the delimeter created
+	var match = input.match(delimeter);
+
+	if(match){
+		// ok, so there's a custom delimeter, split on it and return the sum
+		delimeter = match[1];
+		// reset the inpur sans custom splitter
+		input = match[2];
+		// reset the splitter
+		splitter = new RegExp("[,|\n|" + delimeter + "]");
+	}
+	return input.split(splitter);
+}
+
 function Add(input) {
-	return "";	
+	var nums = parse_input(input);
+	var sum = 0;
+	for (var i = nums.length - 1; i >= 0; i--){
+		// negatives throw error
+		if(nums[i] < 0){
+			throw new Error("ERROR!");
+		}
+
+		sum += parseInt(nums[i], 10);
+	}
+	return sum;
 }
