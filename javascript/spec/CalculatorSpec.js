5ef78fa477a8d072289c196d71647d57dbe2208f
Bryan Grimes <bryan@bryangrimes.com>
2012-04-25 12:18:09 -0400

    commit

--- javascript/spec/CalculatorSpec.js
+++ javascript/spec/CalculatorSpec.js
@@ -1,3 +1,38 @@
 describe("Add", function() {
+  it("empty input retusrns 0", function() {
+    expect("").toReturnSum(0);
+  });
+
+  it("return single input back", function(){
+    expect("1").toReturnSum(1);
+  });
+
+  it("sum of two numbers",function() {
+    expect("1,2").toReturnSum(3);
+  });
+
+  it("sum any amount of numbers", function() {
+    expect("2,4,6,8").toReturnSum(20);
+    expect("1,2,3,4,5,6,7,8,9,10").toReturnSum(55);
+  });
+
+  it("sum, comma and new lines", function(){
+    expect("1\n2,3").toReturnSum(6);
+    expect("1\n2\n3").toReturnSum(6);
+  });
+
+  it("sum with custom delimeter", function() {
+    expect("//@\n1@2@3").toReturnSum(6);
+    expect("//X\n1X2X3").toReturnSum(6);
+  });
+
+  it("negative number error", function(){
+    string num = "-1";
+    expect(function() {Add(num);}).toThrow("negatives not allowed");
+  });
+
+  it("negative numbers are returned in error", function(){
+
+  });
 
 });
--- javascript/src/Calculator.js
+++ javascript/src/Calculator.js
@@ -1,3 +1,31 @@
+function parse_input(input) {
+	if (input === "") return 0;
+
+	// get the delimeter out of the input
+	var delimeter = new RegExp("//(.)\n(.*)");
+	var splitter = new RegExp(/[,|\n]/);
+
+	var match = input.match(delimeter);  // is there a custom delim in the string?
+
+	if(match){
+		delimeter = match[1];
+		input = match[2];
+		// set splitter back
+		splitter = new RegExp("[,|\n|" + delimeter + "]");
+	}
+	return input.split(splitter);
+}
+
 function Add(input) {
-	return "";	
+	var nums = parse_input(input);
+	var sum = 0;
+	for (var i = nums.length - 1; i >= 0; i--){
+		// negatives can't go on....
+		if(nums[i] < 0)
+			//throw {name : "NotImplementedError", message : "negatives not allowed"};
+			throw new Error("negatives not allowed: " + nums[i]);
+
+		sum += parseInt(nums[i], 10);
+	}
+	return sum;
 }
