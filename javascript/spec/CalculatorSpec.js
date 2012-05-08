9464ce7cc6d42139272a7d23f22095b01bbe386c
Bryan Grimes <bryan@bryangrimes.com>
2012-05-07 22:33:06 -0400

    intro of coffeescript

--- javascript/spec/CalculatorSpec.js
+++ javascript/spec/CalculatorSpec.js
@@ -1,3 +1,8 @@
 describe("Add", function() {
-
+	it("should return blank for an empty input", function(){
+		expect("").toReturnSum(0);
+	});
+	it("should return input for no blank input", function(){
+		expect("1").toReturnSum(1);
+	});
 });
---
+++ javascript/src/Calculator.coffee
@@ -0,0 +1,5 @@
+@Add = (input) -> 
+	if input == ""
+		0
+	else
+		parseInt(input, 10)
