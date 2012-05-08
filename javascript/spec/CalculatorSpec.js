498d96c16b3b321f75384ea9c478e8d88c3cb3c4
Bryan Grimes <bryan@bryangrimes.com>
2012-05-08 11:36:08 -0400

    coffee script v1

--- javascript/spec/CalculatorSpec.js
+++ javascript/spec/CalculatorSpec.js
@@ -1,8 +1,47 @@
 describe("Add", function() {
-	it("should return blank for an empty input", function(){
-		expect("").toReturnSum(0);
-	});
-	it("should return input for no blank input", function(){
-		expect("1").toReturnSum(1);
-	});
+  it("empty input returns 0", function() {
+    expect("").toReturnSum(0);
+  });
+
+  it("return single input back", function(){
+    expect("1").toReturnSum(1);
+  });
+
+  it("sum of two numbers",function() {
+    expect("1,2").toReturnSum(3);
+  });
+
+  it("sum any amount of numbers", function() {
+    expect("2,4,6,8").toReturnSum(20);
+    expect("1,2,3,4,5,6,7,8,9,10").toReturnSum(55);
+  });
+
+  it("sum, comma and new lines", function(){
+    expect("1\n2,3").toReturnSum(6);
+    expect("1\n2\n3").toReturnSum(6);
+  });
+
+  it("sum with custom delimeter", function() {
+    expect("//@\n1@2@3").toReturnSum(6);
+    expect("//X\n1X2X3").toReturnSum(6);
+  });
+
+  it("negative number error", function(){
+    var num = "-1";
+    expect(function() {Add(num);}).toThrow(new Error("negatives not allowed: -1"));
+  });
+
+  it("negative numbers are returned in error", function(){
+    var num = "-1,-3";
+    expect(function() {Add(num);}).toThrow(new Error("negatives not allowed: " + num));
+
+    num = "//@\n-1@-2";
+    expect(function() {Add(num);}).toThrow(new Error("negatives not allowed: -1,-2"));
+  });
+
+  it("numbers bigger than 1000 are ignored", function(){
+    expect("5, 1001").toReturnSum(5);
+    expect("5, 1000").toReturnSum(1005);
+  });
+
 });
--- javascript/src/Calculator.coffee
+++ javascript/src/Calculator.coffee
@@ -1,5 +1,28 @@
-@Add = (input) -> 
-	if input == ""
-		0
-	else
-		parseInt(input, 10)
+@Add = (input) ->
+  nums = parse_input(input)
+  sum = 0
+  negatives = []
+  i = nums.length - 1
+
+  while i >= 0
+    negatives.push nums[i]  if nums[i] < 0
+    sum += parseInt(nums[i], 10)  if nums[i] <= 1000
+    i--
+  throw new Error("negatives not allowed: " + negatives.reverse().join())  if negatives.length > 0
+  sum
+
+# return splitted array
+parse_input = (input) ->
+  return 0  if input is ""
+
+  delimeter = new RegExp("//(.*)\n(.*)")
+  splitter = new RegExp(/[,|\n]/)
+  
+  match = input.match(delimeter)
+
+  if match
+    delimeter = match[1]
+    input = match[2]
+    splitter = new RegExp("[,|\n|" + delimeter + "]")
+
+  input.split splitter
